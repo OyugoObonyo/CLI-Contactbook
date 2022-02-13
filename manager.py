cad94b1e93012d692a85f5fd43074bd687449def
OyugoObonyo <nyikal1999@gmail.com>
2022-02-13 12:20:14 +0300

    create the contact manager class

---
+++ manager.py
@@ -0,0 +1,30 @@
+"""
+A module that consists of the contact manager class
+"""
+class ContactManager:
+    """
+    A class that handles actions related to the phonebook
+    """
+    def save(self):
+        """
+        saves a contact to the db
+        """
+        pass
+
+    def show_all(self):
+        """
+        shows all the contacts in the database
+        """
+        pass
+
+    def update(self):
+        """
+        updates a particular contact in the db
+        """
+        pass
+
+    def delete(self):
+        """
+        deletes a contact from the database
+        """
+        pass
