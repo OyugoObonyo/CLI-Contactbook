b40f943047c39365d4f63f5048bf43632a2869bf
OyugoObonyo <nyikal1999@gmail.com>
2022-02-13 20:53:25 +0300

    FEATURE: Add create db to helper functions

--- helpers.py
+++ helpers.py
@@ -1,9 +1,26 @@
 """
 A module with helper functions
 """
+import sqlite3
+
 def exits(boolean):
     """
     changes a booleans value to false
     """
     boolean = False
     return boolean
+
+
+def create_db():
+    conn = sqlite3.connect('contacts.db')
+    conn.execute("""
+    CREATE TABLE IF NOT EXISTS contacts(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    email TEXT)
+    """)
+    
+    return conn
+
+catch = create_db()
+print(catch)
