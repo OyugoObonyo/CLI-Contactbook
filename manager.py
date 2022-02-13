97327bc04d196944bfc67d21816bf3acee366077
OyugoObonyo <nyikal1999@gmail.com>
2022-02-13 21:35:56 +0300

    Add arguements to the contact manager methods

--- manager.py
+++ manager.py
@@ -1,30 +1,59 @@
 """
 A module that consists of the contact manager class
 """
+from helpers import create_db
+
+conn = create_db()
+
 class ContactManager:
     """
-    A class that handles actions related to the phonebook
+    A class that handles actions related to the contacts in the phonebook
     """
-    def save(self):
+    def save(self, name, email):
         """
-        saves a contact to the db
+        save - saves a contact to the db
+        @contact: name of contact to be saved
+        @email: email of contact to be saved
         """
-        pass
+        if name == "" or email == "":
+            print("Name or email cannot be empty")
+            return None
+        conn.execute("INSERT INTO contacts VALUES(?, ?)",(email, name))
+        print("number is saved!")
+
+    def show(self, name):
+        """
+        show - shows a particular contact detail
+        @name: name of contact to be displayed
+        """
+        print("Here is the number!")
+
 
     def show_all(self):
         """
-        shows all the contacts in the database
+        show_all - shows all the contacts in the database
         """
-        pass
+        #remember to include the table library
+        print("all numbers are shown man!")
 
-    def update(self):
+    def update(self, name, new_name, new_email):
         """
         updates a particular contact in the db
+        @name: name of contact to be updated
+        @new_name: new contact name
+        @new_email: new contact email
         """
-        pass
+        print("number is updated!")
 
-    def delete(self):
+    def count_all(self):
         """
-        deletes a contact from the database
+        shows the total count of contacts within the phonebook
         """
-        pass
+        print("50")
+
+    def delete(self, name):
+        """
+        delete - deletes a contact from the database
+        @name: name of contact to be deleted from db
+        """
+        print("number is deleted!")
